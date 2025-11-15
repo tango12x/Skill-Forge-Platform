734792aed5e5b496fba97f7b710e361b7eab9df6
Ahmed Hossam <ahmmedwahba@yahoo.com>
2025-11-16 01:40:47 +0200

    Delete Instructor.java

--- Instructor.java
+++
@@ -1,7 +0,0 @@
-package Backend;
-
-public class Instructor extends User {
-    public Instructor(String userId, String username, String email, String passwordHash) {
-        super(userId, "instructor", username, email, passwordHash);
-    }
-}
