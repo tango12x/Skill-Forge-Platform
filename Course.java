ee37fde3b7ad1ef3c168c2df6d1b650fe539e3d3
Ahmed Hossam <ahmmedwahba@yahoo.com>
2025-11-16 00:40:18 +0200

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,20 @@
+package Backend;
+
+public class Course {
+    private String courseId;
+    private String courseName;
+    private String instructorName;
+    private String description;
+
+    public Course(String courseId, String courseName, String instructorName, String description) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.instructorName = instructorName;
+        this.description = description;
+    }
+
+    public String getCourseId() { return courseId; }
+    public String getCourseName() { return courseName; }
+    public String getInstructorName() { return instructorName; }
+    public String getDescription() { return description; }
+}
