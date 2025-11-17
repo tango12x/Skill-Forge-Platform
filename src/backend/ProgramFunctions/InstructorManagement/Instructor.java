8520ee504c3d3d175ecbff519dace1a3376e0900
AbdelrahmanAtef5172 <abdelrahmanatef426@gmail.com>
2025-11-17 02:31:50 +0200

    Update Instructor.java

--- src/backend/ProgramFunctions/InstructorManagement/Instructor.java
+++ src/backend/ProgramFunctions/InstructorManagement/Instructor.java
@@ -6,12 +6,14 @@ import java.util.ArrayList;
 
 public class Instructor extends User {
     ArrayList<String> createdCourses;
+    //RELATION BETWEEN COURSES AND INSTRUCTOR IS AGGREGATION
 
-    //CLASS CONSTRUCTOR
+    //CLASS CONSTRUCTOR IN CASE OF ID IS GIVEN
     public Instructor(String userId, String username, String email, String passwordHash) {
         super(userId, "instructor", username, email, passwordHash);
         this.createdCourses = new ArrayList<String>();
     }
+    //OVERLOADING CONSTRUCTOR IN CASE THE ID IS NOT GIVEN
     public Instructor( String username, String email, String passwordHash) {
         super("instructor", username, email, passwordHash);
         this.createdCourses = new ArrayList<String>();
